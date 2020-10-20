54c515f567b8440061f8570e71de2e598bcfd59f
Derek <d@deramcreative.com>
2020-10-20 13:25:56 -0400

    Updates

---
+++ precache-manifest.ce7a1fcafee27184959808bc03b4ef5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e0233b2fa7429ea7aba8280e22556d",
+    "url": "/ITEvaluation/index.html"
+  },
+  {
+    "revision": "be0aed3efd0d4fe742c0",
+    "url": "/ITEvaluation/static/css/main.00905ef2.chunk.css"
+  },
+  {
+    "revision": "9be0609564531ee9a4b4",
+    "url": "/ITEvaluation/static/js/2.a704368e.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "/ITEvaluation/static/js/2.a704368e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0aed3efd0d4fe742c0",
+    "url": "/ITEvaluation/static/js/main.88f3e12c.chunk.js"
+  },
+  {
+    "revision": "2101250819c739ac3673",
+    "url": "/ITEvaluation/static/js/runtime-main.101fcab9.js"
+  }
+]);
